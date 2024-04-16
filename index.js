1c18af7ee059c350c4e29be10f680193cdbd9b4d
romainringenbach <5659380+romainringenbach@users.noreply.github.com>
2024-04-16 17:20:22 +0200

    Add evaluate function

--- index.js
+++ index.js
@@ -17,3 +17,55 @@ function divide(a,b){
         return a/b;
     }
 }
+
+function evaluate(operation){
+    let match = operation.match(/^\s*[0-9]+[\.[0-9]*]?\s*[\+|\-|x|\/]\s*[0-9]+[\.[0-9]*]?\s*$/);
+
+    let result = "ERR";
+    let a = null;
+    let b = null;
+    let operator = null;
+    
+    if(match){
+        let operator = null;
+        if(operation.search(/\+/) >= 0){
+            operator = "+";
+        }
+        if(operation.search(/\-/) >= 0){
+            operator = "-";
+        }
+        if(operation.search(/x/) >= 0){
+            operator = "x";
+        }
+        if(operation.search(/\//) >= 0){
+            operator = "/";
+        }
+
+        console.log(operator)
+
+        if(operator){
+            const numbers = operation.split(operator);
+            a = numbers[0].trim();
+            b = numbers[1].trim();
+            
+            switch (operator) {
+                case "+":
+                    result = add(+a,+b);
+                    break;
+                case "-":
+                    result = subtract(+a,+b);
+                    break;
+                case "x":
+                    result = multiply(+a,+b);
+                    break;
+                case "/":
+                    result = divide(+a,+b);
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+    }
+    return result;
+}
